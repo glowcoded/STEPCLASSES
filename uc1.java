48a181b31c84b8810748d727c013d74f32e0ac66
glowcoded <pavithrak7149@gmail.com>
2026-03-02 18:02:58 +0530

    Add files via upload

---
+++ uc1.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp - UC1
+ * Prints "OOPS" to the console.
+ * @author Pavithra
+ * @version 1.0
+ */
+
+public class uc1 {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
